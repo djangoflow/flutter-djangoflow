d0c5b2d42f52bbf165da3813e6bfb094e840d6d4
saiful_apx <saiful@apexive.com>
2023-01-12 23:16:11 +0600

    implement AppCubitBuilder widget

---
+++ packages/djangoflow_app/lib/src/presentation/builders/app_cubit_builder.dart
@@ -0,0 +1,12 @@
+import 'package:djangoflow_app/djangoflow_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppCubitBuilder extends BlocBuilder<AppCubit, AppState> {
+  AppCubitBuilder({
+    super.key,
+    required super.builder,
+    BlocBuilderCondition<AppState>? buildWhen,
+  }) : super(
+          buildWhen: buildWhen ?? (previous, current) => previous != current,
+        );
+}
