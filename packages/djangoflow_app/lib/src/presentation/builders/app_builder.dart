0ce04ab07a43dcdf2c398b2a7fd35c946ff3fa24
saiful_apx <saiful@apexive.com>
2023-01-12 23:15:59 +0600

    Implement AppBuilder widget

---
+++ packages/djangoflow_app/lib/src/presentation/builders/app_builder.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+typedef AppBuilderStateCallBack = Function(BuildContext context);
+
+class AppBuilder extends StatelessWidget {
+  const AppBuilder({
+    super.key,
+    required this.providers,
+    required this.listeners,
+    required this.builder,
+    this.onInitState,
+    this.onDispose,
+  });
+
+  /// Global providers
+  final List<BlocProvider> providers;
+
+  /// Global listeners
+  final List<BlocListener> listeners;
+
+  final AppBuilderStateCallBack? onInitState;
+  final AppBuilderStateCallBack? onDispose;
+
+  /// [WidgetBuilder] is a Flutter framework's callback signature.
+  final WidgetBuilder builder;
+
+  @override
+  Widget build(BuildContext context) => MultiBlocProvider(
+        providers: providers,
+        // to provide current context where initState/dispose can access the providers.
+        child: _AppBuilderStateProvider(
+          onInitState: onInitState,
+          onDispose: onDispose,
+          builder: (context) => MultiBlocListener(
+            listeners: listeners,
+            child: builder(context),
+          ),
+        ),
+      );
+}
+
+class _AppBuilderStateProvider extends StatefulWidget {
+  const _AppBuilderStateProvider(
+      {this.onInitState, this.onDispose, required this.builder});
+
+  /// [WidgetBuilder] is a Flutter framework's callback signature
+  final AppBuilderStateCallBack? onInitState;
+  final AppBuilderStateCallBack? onDispose;
+  final WidgetBuilder builder;
+  @override
+  State<_AppBuilderStateProvider> createState() =>
+      __AppBuilderStateProviderState();
+}
+
+class __AppBuilderStateProviderState extends State<_AppBuilderStateProvider> {
+  @override
+  void initState() {
+    widget.onInitState?.call(context);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.onDispose?.call(context);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(context);
+}
