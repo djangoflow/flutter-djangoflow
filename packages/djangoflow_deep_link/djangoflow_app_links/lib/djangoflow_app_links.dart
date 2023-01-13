0d79ebb3a87b68d79dab45f79754f2eed7317916
saiful_apx <saiful@apexive.com>
2023-01-13 22:40:10 +0600

    implement djangoflow_app_links package

---
+++ packages/djangoflow_deep_link/djangoflow_app_links/lib/djangoflow_app_links.dart
@@ -0,0 +1,4 @@
+library djangoflow_app_links;
+
+export 'src/blocs/djangoflow_app_links_cubit.dart';
+export 'src/data/djangoflow_app_links_repository.dart';
---
+++ packages/djangoflow_deep_link/djangoflow_app_links/lib/src/blocs/djangoflow_app_links_cubit.dart
@@ -0,0 +1,5 @@
+import 'package:djangoflow_deep_link_interface/djangoflow_deep_link_interface.dart';
+
+class AppLinksCubit extends DjangoflowDeepLinkCubitBase<Uri> {
+  AppLinksCubit(super.initialState, super.repository);
+}
