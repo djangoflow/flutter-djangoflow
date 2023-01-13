5f8010c44bcb3fc16f4c8f84e55799b37a988836
saiful_apx <saiful@apexive.com>
2023-01-13 22:04:16 +0600

    update file and class name

--- packages/djangoflow_deep_link/djangoflow_deep_link_interface/lib/src/blocs/djangoflow_deep_link_cubit_base.dart
+++ packages/djangoflow_deep_link/djangoflow_deep_link_interface/lib/src/blocs/djangoflow_deep_link_cubit_base.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'package:djangoflow_deep_link_interface/src/data/djangoflow_deep_link_repository_base.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-abstract class DeepLinkCubitBase<T> extends Cubit<T?> {
-  DeepLinkCubitBase(super.initialState, this.repository);
+abstract class DjangoflowDeepLinkCubitBase<T> extends Cubit<T?> {
+  DjangoflowDeepLinkCubitBase(super.initialState, this.repository);
 
   final DjangoflowDeepLinkRepositoryBase<T> repository;
 
