3782e559aa5aa6b511455cbf397fade32c425782
saiful_apx <saiful@apexive.com>
2023-01-16 23:22:04 +0600

    [djangoflow_error_reporter] todo comment for unit tests

--- packages/djangoflow_error_reporters/djangoflow_error_reporter/test/djangoflow_error_reporter_test.dart
+++ packages/djangoflow_error_reporters/djangoflow_error_reporter/test/djangoflow_error_reporter_test.dart
@@ -1,12 +1,13 @@
-import 'package:flutter_test/flutter_test.dart';
+// TODO create unit-tests
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:djangoflow_error_reporter/djangoflow_error_reporter.dart';
+// import 'package:djangoflow_error_reporter/djangoflow_error_reporter.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+// void main() {
+//   test('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
