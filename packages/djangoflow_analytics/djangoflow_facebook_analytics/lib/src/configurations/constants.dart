9b47d048993029ef05d4ec18ba908118d10290d9
saiful_apx <saiful@apexive.com>
2023-01-18 01:51:15 +0600

    [djangoflow_facebook_analytics] Update configurations

--- packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/src/configurations/constants.dart
+++ packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/src/configurations/constants.dart
@@ -5,3 +5,6 @@ const kMaxSetUserPropertyKeyLength = 24;
 const kMaxSetUserPropertyValueLength = 36;
 
 const kUserIdKey = 'user_id';
+const kEmailKey = 'email';
+const kFirstNameKey = 'first_name';
+const kLastNameKey = 'last_name';
