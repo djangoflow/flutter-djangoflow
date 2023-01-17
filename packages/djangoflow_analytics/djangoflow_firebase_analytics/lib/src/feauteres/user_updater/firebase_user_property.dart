1cd80c5d53a0918c08592f7a5e197cec004b5d17
saiful_apx <saiful@apexive.com>
2023-01-17 17:21:31 +0600

    nullable value for FirebaseUserProperty

--- packages/djangoflow_analytics/djangoflow_firebase_analytics/lib/src/feauteres/user_updater/firebase_user_property.dart
+++ packages/djangoflow_analytics/djangoflow_firebase_analytics/lib/src/feauteres/user_updater/firebase_user_property.dart
@@ -2,7 +2,7 @@ import 'package:analytics/analytics.dart';
 
 class FirebaseUserProperty implements AnalyticAction {
   final String key;
-  final String value;
+  final String? value;
 
   FirebaseUserProperty(this.key, this.value);
 }
