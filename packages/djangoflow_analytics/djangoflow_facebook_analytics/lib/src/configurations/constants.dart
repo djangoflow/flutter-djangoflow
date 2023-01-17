ecdd26d6ac8c8d463d4d520067181d98f8c18f47
saiful_apx <saiful@apexive.com>
2023-01-18 01:24:44 +0600

    add constants

---
+++ packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/src/configurations/constants.dart
@@ -0,0 +1,7 @@
+const kMaxEventKeyLength = 64;
+const kMaxEventValueLength = 100;
+
+const kMaxSetUserPropertyKeyLength = 24;
+const kMaxSetUserPropertyValueLength = 36;
+
+const kUserIdKey = 'user_id';
