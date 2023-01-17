4a5cce792e3c836db7d2ac486742a3e27b5966c9
saiful_apx <saiful@apexive.com>
2023-01-17 17:01:17 +0600

    updated interfaces

---
+++ packages/djangoflow_analytics/djangoflow_analytics/lib/src/data/interfaces/user_property_cutter.dart
@@ -0,0 +1,4 @@
+import 'package:djangoflow_analytics/src/data/interfaces/name_cutter.dart';
+import 'package:djangoflow_analytics/src/data/interfaces/value_cutter.dart';
+
+abstract class UserPropertyCutter implements NameCutter, ValueCutter {}
