240a6bff0b26d9932e544b05cf97d61c82865ef6
saiful_apx <saiful@apexive.com>
2023-01-17 17:01:46 +0600

    export from djangoflow_analytics library

--- packages/djangoflow_analytics/djangoflow_analytics/lib/djangoflow_analytics.dart
+++ packages/djangoflow_analytics/djangoflow_analytics/lib/djangoflow_analytics.dart
@@ -1,3 +1,7 @@
 library djangoflow_analytics;
 
 export 'src/data/analytics.dart';
+export 'src/data/interfaces/event_cutter.dart';
+export 'src/data/interfaces/user_property_cutter.dart';
+export 'src/data/interfaces/has_key.dart';
+export 'src/data/interfaces/has_map_params.dart';
