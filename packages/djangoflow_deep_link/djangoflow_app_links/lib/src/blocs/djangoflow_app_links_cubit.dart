9146a01aa57e9c18f8932e740666981282edd295
saiful_apx <saiful@apexive.com>
2023-01-18 03:01:14 +0600

    [djangoflow_app_links] update files names

--- packages/djangoflow_deep_link/djangoflow_app_links/lib/src/blocs/djangoflow_app_links_cubit.dart
+++
@@ -1,5 +0,0 @@
-import 'package:djangoflow_deep_link_interface/djangoflow_deep_link_interface.dart';
-
-class DjangoflowAppLinksCubit extends DjangoflowDeepLinkCubitBase<Uri> {
-  DjangoflowAppLinksCubit(super.initialState, super.repository);
-}
