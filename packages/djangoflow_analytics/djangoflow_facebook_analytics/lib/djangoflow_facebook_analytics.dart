823063759109934cc47e9e60293ea053ab91518c
saiful_apx <saiful@apexive.com>
2023-01-18 15:45:00 +0600

    fix exports

--- packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/djangoflow_facebook_analytics.dart
+++ packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/djangoflow_facebook_analytics.dart
@@ -3,3 +3,4 @@ library djangoflow_facebook_analytics;
 export 'src/feauteres/user_properties_update/user_properties_update.dart';
 export 'src/feauteres/events/events.dart';
 export 'src/feauteres/user_properties_removal/user_properties_removal.dart';
+export 'src/feauteres/advertise_tracking/advertise_tracking.dart';
--- packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/src/feauteres/advertise_tracking/advertise_tracking.dart
+++ packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/src/feauteres/advertise_tracking/advertise_tracking.dart
@@ -1,2 +1,2 @@
 export 'data/facebook_advertise_tracking_event.dart';
-export 'data/facebook_advertise_tracking_event_updater.dart';
+export 'data/facebook_advertise_tracking_updater.dart';
