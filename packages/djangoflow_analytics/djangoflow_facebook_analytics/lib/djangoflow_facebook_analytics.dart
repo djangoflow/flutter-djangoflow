123abb86b2927f235607fb4dfde40e6d1b57741b
saiful_apx <saiful@apexive.com>
2023-01-23 13:55:24 +0600

    remove todo comment

--- packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/djangoflow_facebook_analytics.dart
+++ packages/djangoflow_analytics/djangoflow_facebook_analytics/lib/djangoflow_facebook_analytics.dart
@@ -4,4 +4,3 @@ export 'src/feauteres/user_properties_update/user_properties_update.dart';
 export 'src/feauteres/events/events.dart';
 export 'src/feauteres/user_properties_removal/user_properties_removal.dart';
 export 'src/feauteres/advertise_tracking/advertise_tracking.dart';
-// TODO update to 0.0.3
