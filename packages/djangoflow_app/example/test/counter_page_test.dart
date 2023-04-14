360da3ed3c7582f103f798b03d73713eb08ed7e8
Tolulope Oke <oketolulope3@gmail.com>
2023-04-14 01:50:25 +0100

    test(counter): add tests for counter page

---
+++ packages/djangoflow_app/example/test/counter_page_test.dart
@@ -0,0 +1,66 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:djangoflow_app/djangoflow_app.dart';
+import 'package:example/bloc/counter_cubit.dart';
+import 'package:example/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'helpers/helpers.dart';
+
+class MockAppCubit extends Mock implements AppCubit {}
+
+class MockCounterCubit extends MockCubit<int> implements CounterCubit {}
+
+void main() {
+  initHydratedStorage();
+
+  group('CounterPage', () {
+    late CounterCubit counterCubit;
+
+    setUp(() {
+      counterCubit = MockCounterCubit();
+    });
+
+    testWidgets('renders current count', (tester) async {
+      const state = 42;
+      when(() => counterCubit.state).thenReturn(state);
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: counterCubit,
+          child: const CounterPage(),
+        ),
+      );
+      expect(find.text('$state'), findsOneWidget);
+    });
+
+    testWidgets('calls increment when increment button is tapped',
+        (tester) async {
+      when(() => counterCubit.state).thenReturn(0);
+      when(() => counterCubit.increment()).thenReturn(null);
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: counterCubit,
+          child: const CounterPage(),
+        ),
+      );
+      await tester.tap(find.byIcon(Icons.add));
+      verify(() => counterCubit.increment()).called(1);
+    });
+
+    testWidgets('calls decrement when decrement button is tapped',
+        (tester) async {
+      when(() => counterCubit.state).thenReturn(0);
+      when(() => counterCubit.decrement()).thenReturn(null);
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: counterCubit,
+          child: const CounterPage(),
+        ),
+      );
+      await tester.tap(find.byIcon(Icons.remove));
+      verify(() => counterCubit.decrement()).called(1);
+    });
+  });
+}
