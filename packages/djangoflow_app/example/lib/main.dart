3ed84c2c70576fadbeb30ab6fd5c9e9badeb5837
Tolulope Oke <oketolulope3@gmail.com>
2023-04-14 01:58:17 +0100

    feat(ui): display djangoflow app snackbar for errors and current environment type

--- packages/djangoflow_app/example/lib/main.dart
+++ packages/djangoflow_app/example/lib/main.dart
@@ -7,32 +7,47 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() => DjangoflowAppRunner.run(
       onException: (exception, stackTrace) {
         // Your custom exception handling
+        final errorMsg = exception.toString();
+        DjangoflowAppSnackbar.showError(errorMsg);
       },
       rootWidgetBuilder: (appBuilder) async {
-        return MaterialApp(
-          title: 'Djangoflow App',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: appBuilder(
-            AppBuilder(
-              repositoryProviders: [
-                // Your repository providers
-              ],
-              providers: [
-                // Your BLoC providers
+        return AppCubitConsumer(
+          listenWhen: (previous, current) =>
+              previous.environment != current.environment,
+          listener: (BuildContext context, AppState state) {
+            final env = state.environment.name;
+            final msg = 'In $env';
+            DjangoflowAppSnackbar.showInfo(msg,
+                duration: const Duration(seconds: 2));
+          },
+          builder: (context, state) {
+            return MaterialApp(
+              title: 'Djangoflow App',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              scaffoldMessengerKey: DjangoflowAppSnackbar.scaffoldMessengerKey,
+              home: appBuilder(
+                AppBuilder(
+                  repositoryProviders: [
+                    // Your repository providers
+                  ],
+                  providers: [
+                    // Your BLoC providers
 
-                // This is a simple counter app, we are making the CounterCubit global for demonstration
-                // In a more complex app, making CounterCubit scoped to the CounterPage would be recommended.
-                BlocProvider<CounterCubit>(create: (_) => CounterCubit()),
-              ],
-              listeners: [
-                // Your BLoC listeners
-              ],
-              builder: (_) => const CounterPage(),
-            ),
-          ),
+                    // This is a simple counter app, we are making the CounterCubit global for demonstration
+                    // In a more complex app, making CounterCubit scoped to the CounterPage would be recommended.
+                    BlocProvider<CounterCubit>(create: (_) => CounterCubit()),
+                  ],
+                  listeners: [
+                    // Your BLoC listeners
+                  ],
+                  builder: (_) => const CounterPage(),
+                ),
+              ),
+            );
+          },
         );
       },
     );
