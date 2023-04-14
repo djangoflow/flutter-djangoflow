6d8f10308fda0964aa94af02eb46b89f9497b711
Tolulope Oke <oketolulope3@gmail.com>
2023-04-14 01:36:12 +0100

    feat: create counter page

---
+++ packages/djangoflow_app/example/lib/counter_page.dart
@@ -0,0 +1,45 @@
+import 'package:example/bloc/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[CounterText()],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: context.read<CounterCubit>().increment,
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            onPressed: context.read<CounterCubit>().decrement,
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterText extends StatelessWidget {
+  const CounterText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final count = context.select((CounterCubit cubit) => cubit.state);
+    return Text('$count', style: theme.textTheme.displayLarge);
+  }
+}
--- packages/djangoflow_app/example/lib/main.dart
+++ packages/djangoflow_app/example/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:djangoflow_app/djangoflow_app.dart';
+import 'package:example/bloc/counter_cubit.dart';
+import 'package:example/counter_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => DjangoflowAppRunner.run(
       onException: (exception, stackTrace) {
@@ -8,70 +11,28 @@ void main() => DjangoflowAppRunner.run(
       rootWidgetBuilder: (appBuilder) async {
         return MaterialApp(
           title: 'Djangoflow App',
+          debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: appBuilder(AppBuilder(
-            repositoryProviders: [
-              // Your repository providers
-            ],
-            providers: [
-              // Your BLoC providers
-            ],
-            listeners: [
-              // Your BLoC listeners
-            ],
-            builder: (context) => const MyHomePage(title: 'Example App'),
-          )),
+          home: appBuilder(
+            AppBuilder(
+              repositoryProviders: [
+                // Your repository providers
+              ],
+              providers: [
+                // Your BLoC providers
+
+                // This is a simple counter app, we are making the CounterCubit global for demonstration
+                // In a more complex app, making CounterCubit scoped to the CounterPage would be recommended.
+                BlocProvider<CounterCubit>(create: (_) => CounterCubit()),
+              ],
+              listeners: [
+                // Your BLoC listeners
+              ],
+              builder: (_) => const CounterPage(),
+            ),
+          ),
         );
       },
     );
-
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
