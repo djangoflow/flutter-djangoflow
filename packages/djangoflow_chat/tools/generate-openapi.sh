40d28823eb5585ef936a36d4c771f5f58a43db05
saiful_apx <saiful@apexive.com>
2023-05-08 17:01:22 +0600

    add generate-openapi.sh

---
+++ packages/djangoflow_chat/tools/generate-openapi.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export TARGET_DIR=./packages/openapi
+export DART_POST_PROCESS_FILE="/usr/local/bin/dartfmt -w"
+export API_VERSION=v1
+cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit
+hostname=${1-https://api.djangoflow-chat.dev}
+
+[ "$1" == "-l" ] && hostname="http://127.0.0.1:8000"
+[ "$1" == "-d" ] && hostname="https://api.djangoflow-chat.dev"
+[ -d ${TARGET_DIR} ] || mkdir -p ${TARGET_DIR}
+${OPENAPI_GENERATOR:-openapi-generator} generate -g dart-dio -p browserClient=false -p nullableFields=true \
+  -p serializationLibrary=json_serializable \
+    -i ${hostname}/api/${API_VERSION}/schema -o ${TARGET_DIR} \
+  # Apply patches here if needed
+  cd ${TARGET_DIR} \
+  && grep -rl 'includeIfNull: truefalse' lib  | xargs sed -i '' 's/includeIfNull: truefalse/includeIfNull: true/g' \
+  && flutter pub get \
+  && flutter pub run build_runner build --delete-conflicting-outputs
