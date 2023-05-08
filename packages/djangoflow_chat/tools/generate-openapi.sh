15cf5420e7e602933308fc4f6a84903e23d183db
saiful_apx <saiful@apexive.com>
2023-05-08 18:52:16 +0600

    update baseurl

--- packages/djangoflow_chat/tools/generate-openapi.sh
+++ packages/djangoflow_chat/tools/generate-openapi.sh
@@ -3,10 +3,10 @@ export TARGET_DIR=./openapi
 export DART_POST_PROCESS_FILE="/usr/local/bin/dartfmt -w"
 export API_VERSION=v1
 cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit
-hostname=${1-https://api.djangoflow-chat.dev}
+hostname=${1-https://api.djangoflow.com}
 
 [ "$1" == "-l" ] && hostname="http://127.0.0.1:8000"
-[ "$1" == "-d" ] && hostname="https://api.djangoflow-chat.dev"
+[ "$1" == "-d" ] && hostname="https://api.djangoflow.com"
 [ -d ${TARGET_DIR} ] || mkdir -p ${TARGET_DIR}
 ${OPENAPI_GENERATOR:-openapi-generator} generate -g dart-dio -p browserClient=false -p nullableFields=true \
   -p serializationLibrary=json_serializable \
