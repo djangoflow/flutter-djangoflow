b17fc40b8e3687110ad879688dfdf90f013e1359
saiful_apx <saiful@apexive.com>
2023-05-09 15:27:52 +0600

    linter fix

--- packages/djangoflow_chat/example/packages/chat/lib/features/authentication/presentation/pages/login_page.dart
+++ packages/djangoflow_chat/example/packages/chat/lib/features/authentication/presentation/pages/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:chat/configurations/configurations.dart';
 import 'package:chat/features/authentication/authentication.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:progress_builder/progress_builder.dart';
 import 'package:reactive_forms/reactive_forms.dart';
