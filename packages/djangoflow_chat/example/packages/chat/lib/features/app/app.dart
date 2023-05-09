70f1fdb5c2e74b47cf945e2bd3eed086266c2e41
saiful_apx <saiful@apexive.com>
2023-05-09 15:39:20 +0600

    update exports of app.dart

--- packages/djangoflow_chat/example/packages/chat/lib/features/app/app.dart
+++ packages/djangoflow_chat/example/packages/chat/lib/features/app/app.dart
@@ -5,3 +5,5 @@ export 'presentation/pages/splash_page.dart';
 export 'presentation/pages/unknown_page.dart';
 export 'presentation/builders/app_responsive_layout_builder.dart';
 export 'presentation/listeners/app_lifecycle_listener.dart';
+export 'presentation/place_holder_text.dart';
+export 'presentation/list_header.dart';
