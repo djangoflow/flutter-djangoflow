44d37a2a2ffd2b4a223e8e6209e1c89bac46427c
saiful_apx <saiful@apexive.com>
2023-05-09 19:18:05 +0600

    show unread messages count and last updated time

--- packages/djangoflow_chat/example/packages/chat/lib/features/chat/presentation/pages/chat_rooms_page.dart
+++ packages/djangoflow_chat/example/packages/chat/lib/features/chat/presentation/pages/chat_rooms_page.dart
@@ -1,7 +1,8 @@
 import 'dart:math';
 
-import 'package:chat/configurations/router/router.dart';
+import 'package:chat/configurations/configurations.dart';
 import 'package:chat/features/app/app.dart';
+import 'package:chat/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_list_bloc/flutter_list_bloc.dart';
@@ -12,39 +13,108 @@ class ChatRoomsPage extends StatelessWidget {
   const ChatRoomsPage({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Chats'),
-          actions: [
-            CircularProgressBuilder(
-              action: (_) => context.read<ChatRoomsListBloc>().create(
-                  roomRequest:
-                      RoomRequest(title: 'Room ${Random().nextInt(100)}')),
-              builder: (context, action, error) => IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: action,
-              ),
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chats'),
+        actions: [
+          CircularProgressBuilder(
+            action: (_) {
+              final randomRoomNumber = Random().nextInt(1000);
+              return context.read<ChatRoomsListBloc>().create(
+                    roomRequest: RoomRequest(
+                      title: 'Room $randomRoomNumber',
+                      description: 'Description for room $randomRoomNumber',
+                      isPublic: true,
+                    ),
+                  );
+            },
+            builder: (context, action, error) => IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: action,
             ),
-          ],
+          ),
+        ],
+      ),
+      body: ListViewBlocBuilder<ChatRoomsListBloc, Room, ChatRoomsListFilter>(
+        withRefreshIndicator: true,
+        emptyBuilder: (_, __) => const Center(
+          child: Text('No chat rooms found'),
         ),
-        body: ListViewBlocBuilder<ChatRoomsListBloc, Room, ChatRoomsListFilter>(
-          emptyBuilder: (_, __) => const Center(
-            child: Text('No chat rooms found'),
-          ),
-          errorBuilder: (_, __) => const Center(
-            child: Text('Failed to load chat rooms'),
-          ),
-          loadingBuilder: (_, __) => const Center(
-            child: ListTileShimmer(),
-          ),
-          itemBuilder: (context, state, index, chatRoom) => ListTile(
-            title: Text(chatRoom.title),
-            subtitle: Text(chatRoom.description ?? ''),
-            onTap: () => chatRoom.id != null
-                ? context.router
-                    .push(ChatMessagesListRoute(roomId: chatRoom.id!))
-                : null,
-          ),
+        errorBuilder: (_, __) => const Center(
+          child: Text('Failed to load chat rooms'),
         ),
-      );
+        loadingBuilder: (_, __) => const Center(
+          child: ListTileShimmer(),
+        ),
+        itemBuilder: (context, state, index, chatRoom) {
+          final lastMessage = chatRoom.lastMessage;
+
+          final lastMessageBody = lastMessage?.body;
+          final isImage =
+              lastMessage?.images != null && lastMessage!.images!.isNotEmpty;
+          return Container(
+            key: ValueKey(chatRoom.id),
+            margin: const EdgeInsets.symmetric(
+              vertical: kPadding,
+              horizontal: kPadding * 2,
+            ),
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(kPadding),
+              ),
+              tileColor: theme.colorScheme.primary.withOpacity(.1),
+              title: Text(chatRoom.title),
+              subtitle: ((lastMessageBody.isNotNullOrEmpty) || isImage == true)
+                  ? Text('"${isImage == true ? "Picture" : lastMessageBody}"')
+                  : null,
+              trailing: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    chatRoom.modified?.toHourMinute() ?? '',
+                    style: textTheme.bodySmall,
+                  ),
+                  const SizedBox(
+                    height: kPadding / 2,
+                  ),
+                  if ((chatRoom.messageNewCount ?? 0) > 0)
+                    Container(
+                      padding: const EdgeInsets.all(kPadding / 2),
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.secondaryContainer,
+                        borderRadius: BorderRadius.circular(kPadding),
+                      ),
+                      child: Text(
+                        chatRoom.messageNewCount.toString(),
+                        style: textTheme.labelSmall,
+                      ),
+                    )
+                ],
+              ),
+              onTap: () => chatRoom.id != null
+                  ? context.router
+                      .push(ChatMessagesListRoute(roomId: chatRoom.id!))
+                      .then((value) {
+                      final chatRoomsListBloc =
+                          context.read<ChatRoomsListBloc>();
+                      final filter = chatRoomsListBloc.state.filter;
+
+                      return chatRoomsListBloc.reload(
+                        filter?.copyWith(
+                          offset: 0,
+                        ),
+                      );
+                    })
+                  : null,
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
