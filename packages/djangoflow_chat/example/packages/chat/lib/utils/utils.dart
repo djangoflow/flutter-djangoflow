6dcbc37bf5668b811062cb014869c546dd2be923
saiful_apx <saiful@apexive.com>
2023-05-09 15:42:42 +0600

    update utils export

--- packages/djangoflow_chat/example/packages/chat/lib/utils/utils.dart
+++ packages/djangoflow_chat/example/packages/chat/lib/utils/utils.dart
@@ -1,3 +1,7 @@
 export 'mixins/mounted_set_state_mixin.dart';
 export 'mixins/cubit_maybe_emit_mixin.dart';
 export 'app_domain.dart';
+export 'extensions/string_extensions.dart';
+export 'extensions/user_extensions.dart';
+export 'extensions/room_user_extensions.dart';
+export 'extensions/message_extensions.dart';
