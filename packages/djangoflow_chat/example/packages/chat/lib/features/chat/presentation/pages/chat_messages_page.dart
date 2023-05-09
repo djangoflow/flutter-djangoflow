989544b93763996bd624311accc790abbea29284
saiful_apx <saiful@apexive.com>
2023-05-09 17:58:00 +0600

    update BlocProviders and add ChatWebsocketCubit in ChatMessagesPages

--- packages/djangoflow_chat/example/packages/chat/lib/features/chat/presentation/pages/chat_messages_page.dart
+++ packages/djangoflow_chat/example/packages/chat/lib/features/chat/presentation/pages/chat_messages_page.dart
@@ -1,27 +1,98 @@
 import 'package:chat/configurations/configurations.dart';
 import 'package:chat/features/app/data/api_repository/api_repository.dart';
+import 'package:chat/features/authentication/blocs/auth_cubit/auth_cubit.dart';
 import 'package:chat/utils/utils.dart';
+import 'package:djangoflow_app/djangoflow_app.dart';
+import 'package:djangoflow_chat/blocs/chat_websocket_cubit/chat_websocket_cubit.dart';
+import 'package:djangoflow_chat/blocs/chat_websocket_cubit/chat_websocket_state.dart';
 import 'package:djangoflow_chat/djangoflow_chat.dart';
 import 'package:djangoflow_chat/models/anonymous_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart' as flutter_chat_ui;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:openapi/openapi.dart';
 
-class ChatMessagesListPage extends StatelessWidget {
+class ChatMessagesListPage extends StatefulWidget with AutoRouteWrapper {
   const ChatMessagesListPage({super.key, @pathParam required this.roomId});
 
   final String roomId;
 
   @override
+  Widget wrappedRoute(BuildContext context) {
+    final token = context.read<AuthCubit>().state.token;
+    final env = context.read<AppCubit>().state.environment;
+
+    return MultiBlocProvider(
+      key: ValueKey(roomId),
+      providers: [
+        BlocProvider<ChatWebsocketCubit>(
+          create: (context) => ChatWebsocketCubit(
+            AppDomain.getCurrentWsUrl(env),
+            token,
+            roomId: roomId,
+          )..connect(),
+        ),
+        BlocProvider<ChatCubit>(
+          create: (context) =>
+              ChatCubit(ApiRepository.instance.chat, roomId: roomId)
+                ..loadData(),
+        ),
+      ],
+      child: this,
+    );
+  }
+
+  @override
+  State<ChatMessagesListPage> createState() => _ChatMessagesListPageState();
+}
+
+class _ChatMessagesListPageState extends State<ChatMessagesListPage>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      context.read<ChatWebsocketCubit>().connect();
+    } else {
+      context.read<ChatWebsocketCubit>().disconnect();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final scheme = theme.colorScheme;
-
-    return BlocProvider<ChatCubit>(
-      key: ValueKey(roomId),
-      create: (context) =>
-          ChatCubit(ApiRepository.instance.chat, roomId: roomId)..loadData(),
+    final websocketConnectionStateMessage = context.select(
+      (ChatWebsocketCubit c) => c.state.connectionStateMessage,
+    );
+    return BlocListener<ChatWebsocketCubit, ChatWebsocketState>(
+      listenWhen: (previous, current) => previous.message != current.message,
+      listener: (context, state) {
+        final chatCubit = context.read<ChatCubit>();
+        final data = state.message;
+        if (data != null) {
+          for (final u in data['users'] ?? []) {
+            final user = RoomUser.fromJson(u);
+            chatCubit.addRoomUser(user);
+          }
+          for (final m in data['messages'] ?? []) {
+            final message = Message.fromJson(m);
+            chatCubit.addMessage(message);
+          }
+        }
+      },
       child: BlocBuilder<ChatCubit, ChatState>(
         builder: (context, chatState) {
           final chatCubit = context.read<ChatCubit>();
@@ -71,9 +142,9 @@ class ChatMessagesListPage extends StatelessWidget {
                   showUserNames: true,
                   showUserAvatars: true,
                   onSendPressed: (messageText) => chatCubit.sendTextMessage(
-                      roomId: roomId, text: messageText.text),
+                      roomId: widget.roomId, text: messageText.text),
                   onEndReached: () =>
-                      chatCubit.loadMoreMessages(roomId: roomId),
+                      chatCubit.loadMoreMessages(roomId: widget.roomId),
                   isAttachmentUploading: chatState.uploadingImage,
                 );
 
@@ -82,7 +153,16 @@ class ChatMessagesListPage extends StatelessWidget {
               title: Text(chatState.room?.title ?? chatState.roomId),
               leading: const AutoLeadingButton(),
             ),
-            body: child,
+            body: Column(
+              children: [
+                if (websocketConnectionStateMessage != null)
+                  Container(
+                    color: scheme.surface,
+                    child: Text(websocketConnectionStateMessage),
+                  ),
+                Expanded(child: child),
+              ],
+            ),
           );
         },
       ),
--- packages/djangoflow_chat/example/packages/chat/lib/utils/app_domain.dart
+++ packages/djangoflow_chat/example/packages/chat/lib/utils/app_domain.dart
@@ -1,3 +1,4 @@
+import 'package:chat/configurations/configurations.dart';
 import 'package:djangoflow_app/djangoflow_app.dart';
 import 'package:flutter/foundation.dart';
 
@@ -6,4 +7,17 @@ class AppDomain {
       (kDebugMode || (kIsWeb && Uri.base.host.endsWith('.dev'))
           ? AppEnvironment.sandbox
           : AppEnvironment.live);
+
+  static String getCurrent(AppEnvironment environment) {
+    final domain =
+        environment == AppEnvironment.live ? kLiveBasePath : kSandboxBasePath;
+
+    return domain;
+  }
+
+  static String getCurrentWsUrl(AppEnvironment environment) {
+    final domain = getCurrent(environment);
+
+    return domain.replaceFirst('http', 'ws');
+  }
 }
