b1db1d7daaf35f0c9cdaadfa971405a300d9d35a
adar2378 <saiful@apexive.com>
2023-08-08 22:18:00 +0600

    update exports

--- packages/djangoflow_auth/lib/djangoflow_auth.dart
+++ packages/djangoflow_auth/lib/djangoflow_auth.dart
@@ -4,3 +4,7 @@ export 'interfaces/social_login.dart';
 export 'blocs/auth_cubit/auth_cubit.dart';
 export 'models/oauth2_configuration.dart';
 export 'browser/web_window.dart';
+export 'social_logins/apple_social_login.dart';
+export 'social_logins/facebook_social_login.dart';
+export 'social_logins/google_social_login.dart';
+export 'social_logins/discord_social_login.dart';
