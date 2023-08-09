a2868bf398ca604494d6d6c971b00c818f6c51e9
adar2378 <saiful@apexive.com>
2023-08-10 02:46:22 +0600

    add additional parameters for FacebookSocialLogin

--- packages/djangoflow_auth/lib/social_logins/facebook_social_login.dart
+++ packages/djangoflow_auth/lib/social_logins/facebook_social_login.dart
@@ -3,8 +3,17 @@ import 'package:djangoflow_openapi/djangoflow_openapi.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class FacebookSocialLogin implements SocialLogin<LoginResult> {
+  final List<String> permissions;
+  final LoginBehavior loginBehavior;
+
+  FacebookSocialLogin(
+      {this.permissions = const ['email', 'public_profile'],
+      this.loginBehavior = LoginBehavior.nativeWithFallback});
   @override
-  Future<LoginResult?> login() async => await FacebookAuth.instance.login();
+  Future<LoginResult?> login() async => await FacebookAuth.instance.login(
+        loginBehavior: loginBehavior,
+        permissions: permissions,
+      );
 
   @override
   Future<void> logout() async {
