7aa71f75a5a897510776648c24563694e7c827da
adar2378 <saiful@apexive.com>
2023-08-10 02:46:07 +0600

    support additional parameters for AppleSocialLogin

--- packages/djangoflow_auth/lib/social_logins/apple_social_login.dart
+++ packages/djangoflow_auth/lib/social_logins/apple_social_login.dart
@@ -3,15 +3,29 @@ import 'package:djangoflow_openapi/djangoflow_openapi.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AppleSocialLogin extends SocialLogin<AuthorizationCredentialAppleID> {
+  final List<AppleIDAuthorizationScopes> scopes;
+  final String? nonce;
+  final WebAuthenticationOptions? webAuthenticationOptions;
+  final String? state;
+
+  AppleSocialLogin(
+      {this.scopes = const [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      this.state,
+      this.nonce,
+      this.webAuthenticationOptions});
+
   @override
   Future<AuthorizationCredentialAppleID?> login() async {
     final isAvailable = await SignInWithApple.isAvailable();
     if (isAvailable) {
       return SignInWithApple.getAppleIDCredential(
-        scopes: [
-          AppleIDAuthorizationScopes.email,
-          AppleIDAuthorizationScopes.fullName,
-        ],
+        scopes: scopes,
+        nonce: nonce,
+        state: state,
+        webAuthenticationOptions: webAuthenticationOptions,
       );
     } else {
       throw Exception('Apple login is not available on this device');
