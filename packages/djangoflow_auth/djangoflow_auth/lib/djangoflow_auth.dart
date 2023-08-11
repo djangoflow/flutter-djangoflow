b3472ef59972d584f3899677c317e8ab50730053
adar2378 <saiful@apexive.com>
2023-08-11 17:28:46 +0600

    export social_login_extensions

--- packages/djangoflow_auth/djangoflow_auth/lib/djangoflow_auth.dart
+++ packages/djangoflow_auth/djangoflow_auth/lib/djangoflow_auth.dart
@@ -5,3 +5,4 @@ export 'blocs/auth_cubit/auth_cubit.dart';
 export 'models/oauth2_configuration/oauth2_configuration.dart';
 export 'models/social_login_type/social_login_type.dart';
 export 'browser/web_window.dart';
+export 'extensions/social_login_extensions.dart';
