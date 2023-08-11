331d57f43a7095e096f6a77102836bfcc4216e44
adar2378 <saiful@apexive.com>
2023-08-11 17:28:13 +0600

    add SocialLoginListExtensions

---
+++ packages/djangoflow_auth/djangoflow_auth/lib/extensions/social_login_extensions.dart
@@ -0,0 +1,18 @@
+import 'package:djangoflow_auth/exceptions/login_provider_not_found_exception.dart';
+import 'package:djangoflow_auth/interfaces/social_login.dart';
+import 'package:djangoflow_openapi/djangoflow_openapi.dart';
+import 'package:collection/collection.dart';
+
+extension SocialLoginListExtensions on List<SocialLogin> {
+  SocialLogin getSocialLoginByProvider(ProviderEnum provider) {
+    final socialLogin = firstWhereOrNull(
+      (element) => element.type.provider == provider,
+    );
+    if (socialLogin == null) {
+      throw LoginProviderNotFoundException(
+          'SocialLogin not found for ${provider.name}\nMake sure you have added the SocialLogin to the list of SocialLogins in the AuthCubit');
+    }
+
+    return socialLogin;
+  }
+}
