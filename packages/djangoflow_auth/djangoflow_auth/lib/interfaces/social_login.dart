dcb9496643027eb936970044db5095333af2cc59
adar2378 <saiful@apexive.com>
2023-08-11 17:28:54 +0600

    update comments

--- packages/djangoflow_auth/djangoflow_auth/lib/interfaces/social_login.dart
+++ packages/djangoflow_auth/djangoflow_auth/lib/interfaces/social_login.dart
@@ -4,14 +4,14 @@ import 'package:djangoflow_auth/models/social_login_type/social_login_type.dart'
 /// The [R] is the type of the response
 
 abstract class SocialLogin<R> {
-  /// The name of the provider
+  /// The type of SocialLogin
   final SocialLoginType type;
 
   SocialLogin({required this.type});
 
-  /// The signIn method
+  /// The login method
   Future<R?> login();
 
-  /// The signOut method
+  /// The logout method
   Future<void> logout();
 }
