90b2ff10f3d4491a0c6d57536a88b7b120b8b6ff
adar2378 <saiful@apexive.com>
2023-08-25 01:40:36 +0600

    refactor: update generate-openapi.sh to support custom hostname

--- packages/djangoflow_openapi_extensions/tools/generate-openapi.sh
+++ packages/djangoflow_openapi_extensions/tools/generate-openapi.sh
@@ -1,18 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 export TARGET_DIR=$(pwd)/packages/djangoflow_openapi
 export DART_POST_PROCESS_FILE="/usr/local/bin/dartfmt -w"
 export API_VERSION=v1
 cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit
-hostname=${1-http://127.0.0.1:8000}
+hostname=${1:-"http://127.0.0.1:8000"}
 
-[ "$1" == "-l" ] && hostname="http://127.0.0.1:8000"
-[ "$1" == "-d" ] && hostname="http://127.0.0.1:8000"
-[ -d ${TARGET_DIR} ] || mkdir -p ${TARGET_DIR}
+echo "Generating OpenAPI client using host: ${hostname}"
+[ -d "${TARGET_DIR}" ] || mkdir -p "${TARGET_DIR}"
 ${OPENAPI_GENERATOR:-openapi-generator} generate -g dart-dio -p browserClient=false -p nullableFields=true \
   -p serializationLibrary=json_serializable -p pubName=djangoflow_openapi \
   -p pubLibrary=djangoflow_openapi \
-    -i ${hostname}/api/${API_VERSION}/schema -o ${TARGET_DIR} \
-  && cd ${TARGET_DIR} \
+  -i "${hostname}/api/${API_VERSION}/schema" -o "${TARGET_DIR}" \
+  && cd "${TARGET_DIR}" \
   && grep -rl 'includeIfNull: truefalse' lib  | xargs sed -i '' 's/includeIfNull: truefalse/includeIfNull: true/g' \
-  && flutter pub upgrade --major-versions \
-  && dart pub run build_runner build --delete-conflicting-outputs
+  && flutter pub get \
+  && dart run build_runner build --delete-conflicting-outputs
