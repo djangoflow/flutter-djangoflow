74f41ea9553a1129e8b561a20e711b52000ca846
adar2378 <saiful@apexive.com>
2023-08-25 01:57:47 +0600

    feat: support API_VERSION as cmd arg

--- packages/djangoflow_openapi_extensions/tools/generate-openapi.sh
+++ packages/djangoflow_openapi_extensions/tools/generate-openapi.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export TARGET_DIR=$(pwd)/packages/djangoflow_openapi
 export DART_POST_PROCESS_FILE="/usr/local/bin/dartfmt -w"
-export API_VERSION=v1
 cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit
-hostname=${1:-"http://127.0.0.1:8000"}
+HOST_NAME=${1:-"http://127.0.0.1:8000"}
+API_VERSION=${2:-"v1"}
 
-echo "Generating OpenAPI client using host: ${hostname}"
+echo "Generating OpenAPI client using host: ${HOST_NAME}"
 [ -d "${TARGET_DIR}" ] || mkdir -p "${TARGET_DIR}"
 ${OPENAPI_GENERATOR:-openapi-generator} generate -g dart-dio -p browserClient=false -p nullableFields=true \
   -p serializationLibrary=json_serializable -p pubName=djangoflow_openapi \
   -p pubLibrary=djangoflow_openapi \
-  -i "${hostname}/api/${API_VERSION}/schema" -o "${TARGET_DIR}" \
+  -i "${HOST_NAME}/api/${API_VERSION}/schema" -o "${TARGET_DIR}" \
   && cd "${TARGET_DIR}" \
   && grep -rl 'includeIfNull: truefalse' lib  | xargs sed -i '' 's/includeIfNull: truefalse/includeIfNull: true/g' \
   && flutter pub get \
