bdcd3bd218b0af8be72b89300e3248e15e37e68f
Ajil Oommen <ajil-apx@apexive.com>
2023-09-21 20:28:16 +0530

    Restructure example to single file

--- packages/djangoflow_app/djangoflow_app_example/lib/main.dart
+++ packages/djangoflow_app/djangoflow_app_example/lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:djangoflow_app/djangoflow_app.dart';
-import 'package:djangoflow_app_example/app.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'utils/app_exception.dart';
 
 void main() {
   DjangoflowAppRunner.run(
     onException: (exception, stackTrace) {
       final errorMessage = switch (exception) {
-        AppException e => e.message,
+        _AppException e => e.message,
         _ => 'An error occurred',
       };
       DjangoflowAppSnackbar.showError(errorMessage);
@@ -31,8 +29,91 @@ void main() {
             },
           ),
         ],
-        builder: (context) => const App(),
+        builder: (context) => AppCubitBuilder(
+          builder: (context, state) => MaterialApp(
+            theme: ThemeData(),
+            scaffoldMessengerKey: DjangoflowAppSnackbar.scaffoldMessengerKey,
+            debugShowCheckedModeBanner: false,
+            darkTheme: ThemeData.dark(),
+            themeMode: state.themeMode,
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('Djangoflow App'),
+                actions: const [
+                  _ToggleThemeModeButton(
+                    key: Key('theme_toggle'),
+                  ),
+                ],
+              ),
+              body: SafeArea(
+                child: ListView(
+                  padding: const EdgeInsets.all(16),
+                  children: [
+                    const _EnvironmentToggle(key: Key('environment_toggle')),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      child: const Text('Throw exception'),
+                      onPressed: () =>
+                          throw const _AppException('Test exception'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            builder: (context, child) => SandboxBanner(
+              isSandbox: state.environment == AppEnvironment.sandbox,
+              child: child ?? const SizedBox.shrink(),
+            ),
+          ),
+        ),
       ),
     ),
   );
+}
+
+class _ToggleThemeModeButton extends StatelessWidget {
+  const _ToggleThemeModeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) => AppCubitBuilder(
+        builder: (context, state) => IconButton(
+          onPressed: () => context.read<AppCubit>().updateThemeMode(
+                switch (state.themeMode) {
+                  ThemeMode.dark => ThemeMode.light,
+                  _ => ThemeMode.dark,
+                },
+              ),
+          icon: Icon(
+            switch (state.themeMode) {
+              ThemeMode.dark => Icons.dark_mode,
+              _ => Icons.light_mode,
+            },
+          ),
+        ),
+      );
+}
+
+class _EnvironmentToggle extends StatelessWidget {
+  const _EnvironmentToggle({super.key});
+
+  @override
+  Widget build(BuildContext context) => AppCubitBuilder(
+        builder: (context, state) => SwitchListTile.adaptive(
+          applyCupertinoTheme: true,
+          value: state.environment == AppEnvironment.live,
+          title: const Text('Live Environment'),
+          onChanged: (_) {
+            context.read<AppCubit>().toggleEnvironment();
+          },
+        ),
+      );
+}
+
+class _AppException implements Exception {
+  const _AppException(this.message);
+
+  final String message;
+
+  @override
+  String toString() => message;
 }
--- packages/djangoflow_app/djangoflow_app_example/lib/utils/app_exception.dart
+++
@@ -1,8 +0,0 @@
-class AppException implements Exception {
-  const AppException(this.message);
-
-  final String message;
-
-  @override
-  String toString() => message;
-}
