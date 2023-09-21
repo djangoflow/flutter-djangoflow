c9924d78c2f3349fcd185a690de1410a86913c6e
Ajil Oommen <ajil-apx>
2023-09-21 18:33:38 +0530

    Add state type
    These listeners won't work if types are not specified

--- packages/djangoflow_app/lib/src/presentation/consumers/app_cubit_consumer.dart
+++ packages/djangoflow_app/lib/src/presentation/consumers/app_cubit_consumer.dart
@@ -4,8 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class AppCubitConsumer extends BlocConsumer<AppCubit, AppState> {
   AppCubitConsumer({
     super.key,
-    BlocBuilderCondition? buildWhen,
-    BlocListenerCondition? listenWhen,
+    BlocBuilderCondition<AppState>? buildWhen,
+    BlocListenerCondition<AppState>? listenWhen,
     required super.builder,
     required super.listener,
   }) : super(
