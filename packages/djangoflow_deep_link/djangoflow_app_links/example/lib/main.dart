b03061c480298d1d7c0ebd8c1b8938e36ff17444
Ajil Oommen <ajil-apx@apexive.com>
2023-09-22 11:36:10 +0530

    Base code setup

---
+++ packages/djangoflow_deep_link/djangoflow_app_links/example/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:djangoflow_app_links/djangoflow_app_links.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final appLinksRepository = AppLinksRepository();
+  final initialLink = await appLinksRepository.getInitialLink();
+
+  runApp(
+    BlocProvider(
+      create: (context) => AppLinksCubit(
+        initialLink,
+        appLinksRepository,
+      ),
+      child: const MainApp(),
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppLinksCubitListener(
+      listener: (context, state) {
+        final path = state?.path;
+        if (path != null) Navigator.of(context).pushNamed(path);
+      },
+      child: MaterialApp(
+        routes: {
+          '/': (context) => const HomePage(),
+          '/primary': (context) => const PrimaryPage(),
+          '/secondary': (context) => const SecondaryPage(),
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Home Page')),
+      );
+}
+
+class PrimaryPage extends StatelessWidget {
+  const PrimaryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Primary Page')),
+      );
+}
+
+class SecondaryPage extends StatelessWidget {
+  const SecondaryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Secondary Page')),
+      );
+}
