b152112863856b55e7edd5fc7bbaa7060615ce4e
Ajil Oommen <ajil-apx@apexive.com>
2023-10-25 13:13:53 +0530

    Add scrollable column base

---
+++ packages/djangoflow_scrollable_column/lib/djangoflow_scrollable_column.dart
@@ -0,0 +1,50 @@
+library djangoflow_scrollable_column;
+
+import 'package:flutter/material.dart';
+
+class DjangoflowScrollableColumn extends StatelessWidget {
+  final ScrollController? controller;
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final bool? primary;
+  final CrossAxisAlignment crossAxisAlignment;
+  final TextDirection? textDirection;
+  final VerticalDirection verticalDirection;
+  final TextBaseline? textBaseline;
+  final List<Widget> children;
+
+  const DjangoflowScrollableColumn({
+    required this.children,
+    Key? key,
+    this.primary,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.textDirection,
+    this.controller,
+    this.verticalDirection = VerticalDirection.down,
+    this.textBaseline,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) => CustomScrollView(
+        controller: controller,
+        primary: primary,
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: mainAxisAlignment,
+              mainAxisSize: mainAxisSize,
+              crossAxisAlignment: crossAxisAlignment,
+              textDirection: textDirection,
+              verticalDirection: verticalDirection,
+              textBaseline: textBaseline,
+              children: children,
+            ),
+          ),
+        ],
+      );
+}
