aad72fefb6dc13fe805d389df98f27126337f142
ajil-apx <145659148+ajil-apx@users.noreply.github.com>
2023-10-26 10:31:47 +0530

    feat(djangoflow_remote_config): Add mapExtensions (#71)
    - Add `removeNullAndCast` to `Map<String, dynamic>`
    - Add tests
    - Add export

--- packages/djangoflow_remote_config/lib/djangoflow_remote_config.dart
+++ packages/djangoflow_remote_config/lib/djangoflow_remote_config.dart
@@ -6,3 +6,4 @@ export 'src/data/models/app_launch_details/app_launch_details.dart';
 export 'src/presentation/remote_config_part_updater.dart';
 export 'src/configuration/constants.dart';
 export 'src/utils/remote_config_extensions.dart';
+export 'src/utils/map_extensions.dart';
---
+++ packages/djangoflow_remote_config/lib/src/utils/map_extensions.dart
@@ -0,0 +1,17 @@
+extension MapExtensions on Map<String, dynamic>? {
+  /// Removes all null values from the map and casts it to a Map<String, Object>
+  Map<String, Object>? removeNullAndCast() {
+    final Map<String, Object> newMap = {};
+    if (this == null || this!.isEmpty) {
+      return null;
+    } else {
+      this!.forEach((key, value) {
+        if (value != null) {
+          newMap[key] = value;
+        }
+      });
+
+      return newMap;
+    }
+  }
+}
