063450400be600c281f5c77d5a8ee1ffe765d167
adar2378 <saiful@apexive.com>
2024-01-03 23:16:14 +0600

    feat: add VideoPlayerWidget

--- packages/djangoflow_video_player/lib/djangoflow_video_player.dart
+++ packages/djangoflow_video_player/lib/djangoflow_video_player.dart
@@ -1,1 +1,4 @@
 library djangoflow_video_player;
+
+export 'src/video_player_widget_builder.dart';
+export 'src/video_player_widget.dart';
--- packages/djangoflow_video_player/lib/src/video_player_widget_builder.dart
+++ packages/djangoflow_video_player/lib/src/video_player_widget_builder.dart
@@ -7,63 +7,89 @@ typedef VideoPlayerWidgetBuilderFunction = Widget Function(
   bool isInitialized,
 );
 
-class VideoPlayerWidgetBuilder extends StatelessWidget {
+typedef VideoPlayerErrorWidgetBuilder = Widget Function(
+  BuildContext context,
+  Object error,
+);
+
+class VideoPlayerWidgetBuilder extends StatefulWidget {
   const VideoPlayerWidgetBuilder({
     required this.builder,
     super.key,
     this.url,
+    this.errorBuilder,
+    this.onInitialize,
   });
+
   final String? url;
   final VideoPlayerWidgetBuilderFunction builder;
+  final VideoPlayerErrorWidgetBuilder? errorBuilder;
+  final Future Function(VideoPlayerController controller)? onInitialize;
 
   @override
-  Widget build(BuildContext context) => _VideoPlayerWidgetInternal(
-        url: url,
-        builder: builder,
-      );
+  State<VideoPlayerWidgetBuilder> createState() =>
+      _VideoPlayerWidgetBuilderState();
 }
 
-class _VideoPlayerWidgetInternal extends StatefulWidget {
-  const _VideoPlayerWidgetInternal({
-    required this.builder,
-    this.url,
-  });
-  final String? url;
-  final VideoPlayerWidgetBuilderFunction builder;
-
-  @override
-  __VideoPlayerWidgetInternalState createState() =>
-      __VideoPlayerWidgetInternalState();
-}
-
-class __VideoPlayerWidgetInternalState
-    extends State<_VideoPlayerWidgetInternal> {
+class _VideoPlayerWidgetBuilderState extends State<VideoPlayerWidgetBuilder> {
   VideoPlayerController? _controller;
   bool _isInitialized = false;
+  Object? _error;
 
   @override
   void initState() {
     super.initState();
+    _initializeAndListen();
+  }
+
+  Future<void> _initializeAndListen() async {
     if (widget.url != null) {
       final uri = Uri.tryParse(widget.url!);
       if (uri != null) {
         _controller = VideoPlayerController.networkUrl(uri)
-          ..initialize().then((_) {
+          ..initialize().then((_) async {
+            if (widget.onInitialize != null) {
+              await widget.onInitialize!(_controller!);
+            }
             setState(() {
               _isInitialized = true;
             });
+          }).catchError((error) {
+            setState(() {
+              _error = error;
+            });
+            debugPrint('Error initializing video player: $error');
           });
+
+        _controller?.addListener(_listener);
       }
     }
   }
 
+  void _listener() {
+    setState(() {});
+  }
+
   @override
   void dispose() {
+    _controller?.removeListener(_listener);
     _controller?.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) =>
-      widget.builder(context, _controller, _isInitialized);
+  Widget build(BuildContext context) {
+    if (_error != null) {
+      if (widget.errorBuilder != null) {
+        return widget.errorBuilder!(context, _error!);
+      } else {
+        return const Center(
+          child: Icon(
+            Icons.error,
+          ),
+        );
+      }
+    }
+    return widget.builder(context, _controller, _isInitialized);
+  }
 }
