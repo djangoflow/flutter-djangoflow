1db8de56925e53013210698338b624d2bf28507c
adar2378 <saiful@apexive.com>
2024-01-06 02:01:18 +0600

    refactor: add exmaple app

---
+++ packages/djangoflow_video_player/example/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:djangoflow_video_player/djangoflow_video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Djangoflow Video Player Example',
+      home: VideoPlayerScreen(),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatelessWidget {
+  const VideoPlayerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Djangoflow Video Player'),
+      ),
+      body: SizedBox(
+        height: 400,
+        child: VideoPlayerWidget(
+          url:
+              'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', // Replace with a valid video URL
+        ),
+      ),
+    );
+  }
+}
