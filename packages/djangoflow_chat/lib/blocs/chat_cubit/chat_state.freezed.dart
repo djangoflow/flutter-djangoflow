cada8a1f3041dd91220ef9bcc555775b5f967474
adar2378 <saiful@apexive.com>
2024-04-08 17:19:04 +0600

    refactor: update freezed generated files

--- packages/djangoflow_chat/lib/blocs/chat_cubit/chat_state.freezed.dart
+++ packages/djangoflow_chat/lib/blocs/chat_cubit/chat_state.freezed.dart
@@ -12,7 +12,7 @@ part of 'chat_state.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 ChatState _$ChatStateFromJson(Map<String, dynamic> json) {
   return _ChatState.fromJson(json);
@@ -210,7 +210,7 @@ class _$ChatStateImpl implements _ChatState {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$ChatStateImpl &&
