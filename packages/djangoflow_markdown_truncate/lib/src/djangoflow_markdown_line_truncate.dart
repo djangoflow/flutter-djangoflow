242da8a2c334363c4b9385f5e26cf66dfc34b986
adar2378 <saiful@apexive.com>
2024-08-22 17:49:33 +0600

    refactor: add simplified max line support

--- packages/djangoflow_markdown_truncate/lib/src/djangoflow_markdown_line_truncate.dart
+++ packages/djangoflow_markdown_truncate/lib/src/djangoflow_markdown_line_truncate.dart
@@ -1,12 +1,11 @@
-import 'package:djangoflow_markdown_truncate/src/base_markdown_truncate.dart';
 import 'package:flutter/material.dart';
+import 'package:djangoflow_markdown_truncate/src/base_markdown_truncate.dart';
 
 class DjangoflowMarkdownLineTruncate extends BaseMarkdownTruncate {
   const DjangoflowMarkdownLineTruncate({
     required super.data,
     super.key,
     this.maxLines,
-    this.overflow = TextOverflow.ellipsis,
     super.selectable,
     super.styleSheet,
     super.styleSheetTheme,
@@ -23,97 +22,77 @@ class DjangoflowMarkdownLineTruncate extends BaseMarkdownTruncate {
     super.listItemCrossAxisAlignment,
     super.shrinkWrap,
     super.fitContent,
-    super.onReadMoreTapped,
   });
 
   final int? maxLines;
-  final TextOverflow overflow;
-
-  Widget buildLineLimitedRichText(
-      List<Text> textWidgets, BoxConstraints constraints) {
-    final combinedSpan = TextSpan(
-      children: textWidgets
-          .map((textWidget) => textWidget.textSpan)
-          .whereType<InlineSpan>()
-          .toList(),
-    );
-
-    final textPainter = TextPainter(
-      text: combinedSpan,
-      maxLines: maxLines,
-      textDirection: TextDirection.ltr,
-    );
-
-    textPainter.layout(maxWidth: constraints.maxWidth);
-
-    if (textPainter.didExceedMaxLines) {
-      final truncatedSpans = <InlineSpan>[];
-      var currentOffset = 0;
-
-      for (final span in combinedSpan.children!) {
-        if (span is TextSpan) {
-          final spanTextLength = span.toPlainText().length;
-          final lastPosition = textPainter
-              .getPositionForOffset(
-                Offset(constraints.maxWidth, textPainter.height),
-              )
-              .offset;
-
-          if (currentOffset + spanTextLength > lastPosition) {
-            final remainingLength = lastPosition - currentOffset;
-            if (remainingLength > 0) {
-              truncatedSpans.add(TextSpan(
-                text: span.text?.substring(0, remainingLength) ?? '',
-                style: span.style,
-              ));
-            }
-            truncatedSpans
-                .addAll(buildReadMoreSpan(styleSheet?.p, styleSheet?.a));
-            break;
-          } else {
-            truncatedSpans.add(span);
-          }
-
-          currentOffset += spanTextLength;
-        }
-      }
-
-      return RichText(
-        text: TextSpan(children: truncatedSpans),
-        textAlign: TextAlign.start,
-        softWrap: true,
-      );
-    } else {
-      return RichText(
-        text: combinedSpan,
-        textAlign: TextAlign.start,
-        softWrap: true,
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context, List<Widget>? children) {
-    if (maxLines == null || children == null || children.isEmpty) {
+    if (maxLines == null) {
       return ListView(
         shrinkWrap: shrinkWrap,
-        children: children ?? [],
+        children: children!,
       );
     }
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final textWidgets = <Text>[];
-        for (final child in children) {
-          textWidgets.addAll(findWidgetsOfType<Text>(child));
-        }
+    final flattenedTextSpan = _flattenTextSpan(children);
 
-        if (textWidgets.isNotEmpty) {
-          return buildLineLimitedRichText(textWidgets, constraints);
-        } else {
-          return Container();
-        }
-      },
+    return ListView(
+      shrinkWrap: shrinkWrap,
+      children: [
+        Text.rich(
+          flattenedTextSpan,
+          maxLines: maxLines,
+          overflow: TextOverflow.ellipsis, // No ellipsis here, handled manually
+        ),
+      ],
     );
+  }
+
+  TextSpan _flattenTextSpan(List<Widget>? widgets) {
+    final textSpans = <InlineSpan>[];
+
+    for (final widget in widgets!) {
+      if (widget is Text) {
+        final textSpan = widget.textSpan ?? TextSpan(text: widget.data);
+        textSpans.add(textSpan);
+      } else if (_hasChildren(widget)) {
+        final children = _getChildren(widget);
+        textSpans.addAll(_flattenTextSpan(children).children ?? []);
+      }
+    }
+
+    return TextSpan(children: textSpans);
+  }
+
+  bool _hasChildren(Widget widget) =>
+      widget is Wrap ||
+      widget is Column ||
+      widget is Row ||
+      widget is SizedBox ||
+      widget is Flexible ||
+      widget is Expanded ||
+      widget is Padding ||
+      widget is Container;
+
+  List<Widget> _getChildren(Widget widget) {
+    if (widget is Wrap) {
+      return widget.children;
+    } else if (widget is Column) {
+      return widget.children;
+    } else if (widget is Row) {
+      return widget.children;
+    } else if (widget is SizedBox && widget.child != null) {
+      return [widget.child!];
+    } else if (widget is Flexible) {
+      return [widget.child];
+    } else if (widget is Expanded) {
+      return [widget.child];
+    } else if (widget is Padding && widget.child != null) {
+      return [widget.child!];
+    } else if (widget is Container && widget.child != null) {
+      return [widget.child!];
+    }
+    return [];
   }
 }
