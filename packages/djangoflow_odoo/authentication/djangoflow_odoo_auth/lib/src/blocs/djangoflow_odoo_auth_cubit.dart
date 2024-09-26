93fcaa78c9a4acb28976e7889d5e97ccd855fd52
adar2378 <saiful@apexive.com>
2024-09-26 13:18:06 +0600

    refactor: update djangoflow_odoo_auth_cubit.dart imports and emit statements

--- packages/djangoflow_odoo/authentication/djangoflow_odoo_auth/lib/src/blocs/djangoflow_odoo_auth_cubit.dart
+++ packages/djangoflow_odoo/authentication/djangoflow_odoo_auth/lib/src/blocs/djangoflow_odoo_auth_cubit.dart
@@ -1,12 +1,10 @@
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:odoo_rpc/odoo_rpc.dart';
 
-import '../repositories/djangoflow_odoo_auth_repository.dart';
-import 'djangoflow_odoo_auth_state.dart';
+import 'package:djangoflow_odoo_auth/src/repositories/djangoflow_odoo_auth_repository.dart';
+import 'package:djangoflow_odoo_auth/src/blocs/djangoflow_odoo_auth_state.dart';
 
 class DjangoflowOdooAuthCubit extends HydratedCubit<DjangoflowOdooAuthState> {
-  final DjangoflowOdooAuthRepository _repository;
-
   DjangoflowOdooAuthCubit(this._repository)
       : super(const DjangoflowOdooAuthState()) {
     if (state.baseUrl != null && state.session != null) {
@@ -14,28 +12,35 @@ class DjangoflowOdooAuthCubit extends HydratedCubit<DjangoflowOdooAuthState> {
     }
     _repository.sessionStream.listen(_handleSessionChange);
   }
+  final DjangoflowOdooAuthRepository _repository;
 
   void _handleSessionChange(OdooSession session) {
     if (session.id.isEmpty || session.userId == 0) {
       logout();
     } else {
-      emit(state.copyWith(
-        status: AuthStatus.authenticated,
-        session: session,
-      ));
+      emit(
+        state.copyWith(
+          status: AuthStatus.authenticated,
+          session: session,
+        ),
+      );
     }
   }
 
   Future<void> checkAuthStatus() async {
     final storedSession = state.session;
     if (storedSession != null && state.baseUrl != null) {
-      await _repository.initializeClient(state.baseUrl!,
-          session: storedSession);
+      await _repository.initializeClient(
+        state.baseUrl!,
+        session: storedSession,
+      );
       final isValid = await _repository.validateSession();
       if (isValid) {
-        emit(state.copyWith(
-          status: AuthStatus.authenticated,
-        ));
+        emit(
+          state.copyWith(
+            status: AuthStatus.authenticated,
+          ),
+        );
       } else {
         emit(
           state.copyWith(
@@ -65,34 +70,42 @@ class DjangoflowOdooAuthCubit extends HydratedCubit<DjangoflowOdooAuthState> {
 
   Future<void> login(String username, String password) async {
     if (state.baseUrl == null || state.database == null) {
-      emit(state.copyWith(
+      emit(
+        state.copyWith(
           status: AuthStatus.error,
-          errorMessage: 'Base URL or database not set'));
+          errorMessage: 'Base URL or database not set',
+        ),
+      );
       return;
     }
     try {
       final session =
           await _repository.login(state.database!, username, password);
-      emit(state.copyWith(
-        status: AuthStatus.authenticated,
-        session: session,
-      ));
+      emit(
+        state.copyWith(
+          status: AuthStatus.authenticated,
+          session: session,
+        ),
+      );
       // The session change will be handled by the session stream listener
     } catch (e) {
       emit(
-          state.copyWith(status: AuthStatus.error, errorMessage: e.toString()));
+        state.copyWith(status: AuthStatus.error, errorMessage: e.toString()),
+      );
     }
   }
 
   Future<void> logout() async {
     await _repository.logout();
-    emit(state.copyWith(
-      status: AuthStatus.unauthenticated,
-      baseUrl: null,
-      database: null,
-      errorMessage: null,
-      session: null,
-    ));
+    emit(
+      state.copyWith(
+        status: AuthStatus.unauthenticated,
+        baseUrl: null,
+        database: null,
+        errorMessage: null,
+        session: null,
+      ),
+    );
   }
 
   @override
