bb5a5a4b65cd9687c09af96153e483bfeac8e5a6
adar2378 <saiful@apexive.com>
2025-05-21 15:26:19 +0600

    chore: example app update

--- packages/djangoflow_markdown_truncate/example/lib/main.dart
+++ packages/djangoflow_markdown_truncate/example/lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
@@ -28,6 +30,8 @@ class HomePage extends StatelessWidget {
 ##### h5 Heading
 ###### h6 Heading
   ''';
+
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
